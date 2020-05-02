c143c86ef25a88d712fde81e706dc1ec2cc00db0
Cyrill Burgener <cyrill.burgener@gmail.com>
2020-05-02 10:36:52 +0200

    Commit

--- scripts/libjpeg-turbo.sh
+++ scripts/libjpeg-turbo.sh
@@ -5,7 +5,7 @@ tar xf 2.0.4.tar.gz
 cd libjpeg-turbo-2.0.4
 mkdir build && cd build
 
-emconfigure cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$EMSCRIPTEN/system
+emcmake cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$EMSCRIPTEN/system
 
 emmake make -j
 emmake make -j install
